b1dd98cdf9aec8482ceeb6837dc983eea05eb86a
karinashin <karinashin0531@gmail.com>
2022-04-10 16:50:00 -0500

    rapidJSON???

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include "include/rapidjson/document.h"
 
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    rapidjson::Document doc;
+    ifstream stream;
+    stream.open("blogs_0000001.json");
+    if (!stream.is_open())
+        cout << "Not open" << endl;
+//
+//    string wholeFile;
+//    string temp;
+//    while (getline(stream, temp))//not reading anything
+//    {
+//        cout << temp << endl;
+//        wholeFile += temp;
+//    }
+//    stream.close();
+//
+//    doc.Parse(wholeFile.c_str());
+//    if (doc.IsObject()) cout << "ITS AN OBJECT" << endl;
+//
+//    string notArr = doc["root"]["entities"]["persons"][0]["name"].GetString();
+//    cout << "notArr: " << notArr << endl;
+//
+//    doc["root"]["entities"]["persons"].IsObject();
+//    doc["root"]["entities"]["persons"].IsArray();
+//    for (auto& v : doc["root"]["entities"]["persons"].GetArray())
+//        cout << "Value: " << v.GetString() << endl;
 }
